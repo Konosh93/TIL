4103fbf36f3e373eb8dea2c569737b80f7396133
Omer <omer.rayah@gmail.com>
2017-08-29 13:37:03 +0900

    added models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post (models.Model):
+	title = models.CharField(max_length=255,default="Today I learned ...")
+	author = models.ForeignKey(User)
+	text = models.CharField(max_length=255,blank=True)
+	date_created = models.DateTimeField(auto_now_add=True)
+	tags = models.CharField(max_length=255)
+	likes = models.IntegerField(default=0)
+
+	class Meta:
+		ordering = ("-date_created",) 
+
+class Comment(models.Model):
+	author = models.ForeignKey(User)
+	post = models.ForeignKey(Post)
+	text = models.CharField(max_length=255,blank=True)
+	date_created = models.DateTimeField(auto_now_add=True)
+	likes = models.IntegerField(default=0)
+
+	class Meta:
+		ordering = ("date_created",) 
--- til/settings.py
+++ til/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts.apps.postsConfig',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
